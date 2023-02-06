0f08cab7aab8bf9bb0c1718927e31cf9f4a2fded
JordanTate <JordanTateAdds@iCloud.com>
2023-02-06 19:27:31 +0000

    Setup a simple server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+// Configure Enviromental Variables
+require('dotenv').config();
+
+// Packages
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Set Express App
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Configure Port
+const PORT = process.env.PORT || 3001;
+
+// Connect to Database
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        // Listen
+        app.listen(PORT, () => {
+            console.log(`Connected to Database. Servier listening on PORT: ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
